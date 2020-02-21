67812cd03416d8eb3958e9d79a1b91a540200f3f
yogeshwar <yogeshwar@prodiodesignworks.in>
2020-02-21 14:22:55 +0530

    added ssh key

---
+++ src/component/UrlFilter.jsx
@@ -0,0 +1,34 @@
+
+
+
+
+
+/*import React from 'react';
+import './App.css';
+import User from './component/User.component';
+
+function UrlFilter() {
+
+  const useFetch = url => {
+    const [data, setData] = useState([]);
+
+  }
+
+    React.useEffect(() => {
+    const response =  fetch(url);
+    const data =  response.json();
+    const [item] = data.results;
+    setData(item);
+      }, []);
+
+      const {url,setUrl} = useFetch(this.props.url)
+  return (
+    
+    <div>
+      <User name = {setData}/>
+    </div>
+  );
+}
+
+export default UrlFilter;
+*/
---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+
+/*import React from 'react';
+import User from '../component/User.component'
+
+
+
+function Home(){
+
+    return(
+    <div>
+        <User name = {this.this.props.} />
+    </div>
+    );
+    
+
+}
+
+export default Home;
+*/
